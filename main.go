74a8114a0247eb48426293950d1c38fcf8ac7763
AdimekweEbuka <adimekwe.ebuka@gmail.com>
2021-02-19 01:19:29 +0100

    tcas now works

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import ( 
+	"fmt"
+	"math/rand"
+	"time"
+)
+const (
+	minimumAltitude = 1000
+	maximumAltitude = 1550
+	minimumBearing = 0
+	maximumBearing = 60
+)
+
+// Airport is an airport struct
+type Airport struct {
+	name string
+}
+
+// Airplane is an airplane struct
+type Airplane struct {
+	name string
+	callSign string
+	altitude int
+	bearing int
+	hasTaxied 	bool
+}
+
+func (a *Airplane) taxi() {
+	fmt.Println(a.callSign, "you have permission to taxi!")
+	a.hasTaxied = true
+}
+
+func (a *Airplane) takeOff() {
+	
+	if a.hasTaxied {
+		fmt.Println(a.callSign, "you have permission to takeoff!")
+		rand.Seed(time.Now().UnixNano())
+		a.bearing = generateRandomInt(minimumBearing, maximumBearing)
+
+		a.altitude = generateRandomInt(minimumAltitude, maximumAltitude)
+	
+		
+	} else {
+		fmt.Println(a.callSign, "please proceed to the taxi station")
+	}
+}
+
+func generateRandomInt(min, max int) int {
+	return (rand.Intn(max - min + 1) + min)
+}
+
+func initiateTCAS(airplaneOne, airplaneTwo Airplane) {
+	// var collisionCourse bool
+
+	initiate:
+	if airplaneOne.altitude == airplaneTwo.altitude && airplaneOne.bearing == airplaneTwo.bearing {
+		fmt.Println("WARNING!!! COLLISION AHEAD!!!")
+		//collisionCourse = true
+	}
+	rand.Seed(time.Now().UnixNano())
+	airplaneOne.bearing = generateRandomInt(minimumBearing, maximumBearing)
+	airplaneTwo.bearing = generateRandomInt(minimumBearing, maximumBearing)
+	airplaneOne.altitude = generateRandomInt(minimumAltitude, maximumAltitude)
+	airplaneTwo.altitude = generateRandomInt(minimumAltitude, maximumAltitude)
+	goto initiate
+	// for collisionCourse == false { 
+	// 	if airplaneOne.altitude == airplaneTwo.altitude && airplaneOne.bearing == airplaneTwo.bearing {
+	// 		fmt.Println("WARNING!!! COLLISION AHEAD!!!")
+	// 		collisionCourse = true
+	// 	}
+	// }
+}
+
+// func generateRandomFloat(min, max float64) float64 {
+// 	return (min + rand.Float64() * (max - min))
+// }
+
+// func roundUp(number float64) float64 {
+// 	return (math.Floor(number * 100) / 100)
+// }
+
+func main() {
+	airplane1 := Airplane {
+		name: "Emirates",
+		callSign: "EMR076",
+	}
+
+	airplane2 := Airplane {
+		name: "Air Peace",
+		callSign: "AP327",
+	}
+
+	airplane1.taxi()
+	airplane1.takeOff()
+
+	airplane2.taxi()
+	airplane2.takeOff()
+
+	initiateTCAS(airplane1, airplane2)
+}
